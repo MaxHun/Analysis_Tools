65f20951b59308dba7059996a7d64dd6946b19c9
Max Huneshagen <maxhuneshagen@gmail.com>
2018-08-06 20:22:13 +0200

    Updated merging tool for WL

---
+++ WangLandau/mergeHGLnDOS.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#array=$(ls | grep -a 'final_HGLnDOS')
+array=(../data/WangLandau/*final_HGLnDOS*)
+
+echo $array
+# get length of an array
+arraylength=${#array[@]}
+
+
+python3 OverlapAndAverage.py ${array[0]} ${array[1]} "out.dat"
+
+for (( i=2; i<${arraylength}; i++ ));
+do
+  echo $i " / " ${arraylength} " : " ${array[$i]}
+  winOne="out.dat" #${array[$i]}
+  winTwo=${array[$i]}
+  winOut="out.dat"
+  
+  python3 OverlapAndAverage.py $winOne $winTwo $winOut
+  
+done
+
+datname="out.dat" ${array[0]%.bfm*}"_HGLnDOS_shifted.dat"
+echo $name
+python3 ShiftToZero.py "out.dat" "output.dat" #${name:19}
+echo ${array[0]%.bfm*:19}"_HGLnDOS_shifted.dat"
+
+
+#echo ${array[0]%.bfm*}"_HGLnDOS_shifted.dat"
+
