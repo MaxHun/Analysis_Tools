a68e0101b42550869c4af48670b1b5d45680c6db
Max Huneshagen <maxhuneshagen@gmail.com>
2018-09-12 19:39:15 +0200

    AI

--- AdsorptionIsotherm/AI_contacts_by_ninshell_plot.py
+++ AdsorptionIsotherm/AI_contacts_by_ninshell_plot.py
@@ -41,20 +41,21 @@ for File in files:
         except:
             nc = int(File[8:10])
     epsplot = -np.loadtxt(File, unpack=True)[0]
-    phiplot = np.loadtxt(File, unpack=True)[5]
-    ax.plot(epsplot,phiplot,label=r"$N_c={}$".format(nc),
+    nisplot = np.loadtxt(File, unpack=True)[8]
+    nconplot = np.loadtxt(File, unpack=True)[4] # Anzahl der Kontakte
+    ax.plot(epsplot,nconplot/nisplot,label=r"$N_c={}$".format(nc),
             color=colors[k],dashes=ls_dashes[k],
             lw=0,ls="", marker=markers[k],ms=15)
     ax.set_xlabel(r"$-\epsilon$",fontsize=fontsize_label)
-    ax.set_ylabel(r"$\phi$",fontsize=fontsize_label)
+    ax.set_ylabel(r"$\frac{n_{Contacts}}{n_{c,Shell}}$",fontsize=fontsize_label)
     k+=1
     for n in np.arange(len(sys.argv)):
         if sys.argv[n]=="dat":
-            data = np.transpose(np.array([phiplot,epsplot]))
-            np.savetxt("phi_eps_Nc{}.dat".format(nc), data, delimiter= "     ", fmt="%-1.8f",
-                       header="phi            epsilon")
+            data = np.transpose(np.array([nconplot/nisplot,epsplot]))
+            np.savetxt("n_c_by_n_shell_Nc{}.dat".format(nc), data, delimiter= "     ", fmt="%-1.8f",
+                       header="<(nContacts>/<nCoS In NNShell>            epsilon")
     #try to fit the data:
-    popt, pcov = curve_fit(ai, epsplot, phiplot)
+    popt, pcov = curve_fit(ai, epsplot, nisplot)
     #plt.plot(-epsplot, ai(-epsplot, *popt), label="fit")
 
 ax.tick_params(left=True,right=True,bottom=True,top=True,which='major',length=10)
@@ -72,13 +73,13 @@ minor_locator_x = AutoMinorLocator(2)
 ax.xaxis.set_minor_locator(minor_locator_x)
 #ax.yaxis.set_minor_locator(minor_locator_y)
 #plt.yticks(plt.yticks()[0][::2]) # jeden zweiten Tick löschen
-plt.xticks([0,2,4,6,8])
+#plt.xticks([0,2,4,6,8])
 ax.set_xlim(0,8)
-ax.set_ylim(0,1)
+ax.set_ylim(2.6,4.1)
 plt.subplots_adjust(left=0.07,right=0.98,top=0.98,bottom=0.09)
 ax.legend(loc='upper left', prop={'size': fontsize})
 for i in np.arange(len(sys.argv)):
     if sys.argv[i] == "png":
-        plt.savefig("../../ownCloud/SS18/BA/Vortrag/Bilder/AI_plot.png")
+        plt.savefig("../../ownCloud/SS18/BA/Vortrag/Bilder/AI_n_c_by_n_shell_plot.png")
 plt.show()
 
