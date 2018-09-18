1cf4bd76e5f770a4ea7424f6b6ebeb4b91cfa3a4
Max Huneshagen <maxhuneshagen@gmail.com>
2018-09-18 21:59:53 +0200

    .

--- WangLandau/CoSolvent/N64/Psi_T/psi_T_plot.py
+++ WangLandau/CoSolvent/N64/Psi_T/psi_T_plot.py
@@ -21,30 +21,22 @@ markers = np.array(["o","v","s","+","*","p","x"])
 linestyles = np.array([":","-.","--","-"])
 ls_dashes = np.array([[3,3,1,1],[1,1],[1,3,3,1],[2,4,2,4,2,8],[2,2,10,2],[],[4,4,2,2],[3,1,3,1],[2,4,4,2],[5,1,5,1],[7,1,7,1],[7,3,7,3],[3,1,1,1,1,1]])
 #print(files)
-files = glob.glob("phi_T*.dat")
+files = glob.glob("psi_T_*.dat")
 files.sort()
 eps_WL=-0.4
 f, ax =plt.subplots(1,1, figsize=(20,10))
 k=0
-epslice=False
-for i in np.arange(len(sys.argv)):
-    if sys.argv[i] == "epslice":
-        epslice=True
-        eps_slice=float(sys.argv[i+1])
-        #print(eps_slice)
-c_slice=np.zeros(len(files))
-Rg2_slice=np.zeros(len(files))
 for File in files:
     Tplot = np.loadtxt(File, unpack=True)[0]
-    phiplot = np.loadtxt(File, unpack=True)[1]
+    psiplot = np.loadtxt(File, unpack=True)[1]
     epsplot = eps_WL/Tplot
     c=float(File[-9:-4])
 
-    ax.plot(epsplot, phiplot,color=colors[k%len(colors)], alpha=0.6, 
+    ax.plot(epsplot, psiplot,color=colors[k%len(colors)], alpha=0.6, 
             dashes=ls_dashes[k%len(ls_dashes)], label=r"$c={}$".format(c),
             lw=3)
     plt.xlabel(r"$\epsilon_{ME}=\epsilon_{WL}\cdot T^{-1}$",fontsize=fontsize_label)
-    plt.ylabel(r"$\phi$",fontsize=fontsize_label)
+    plt.ylabel(r"$\psi$",fontsize=fontsize_label)
     #plt.xlim(10**-3,10**0)
     #ax.yaxis.set_label_coords(-0.05,0.5)
     ax.tick_params(left=True,right=True,bottom=True,top=True,which='major',length=10)
@@ -63,8 +55,9 @@ for File in files:
 
 for i in np.arange(len(sys.argv)):
     if sys.argv[i] == "png":
-        plt.savefig("../../../../../ownCloud/SS18/BA/Vortrag/Bilder/CNS_phi_T.png")
-#plt.yticks(plt.yticks()[0][::2]) # jeden zweiten Tick löschen
+        plt.savefig("../../../../../ownCloud/SS18/BA/Vortrag/Bilder/CNS_psi_T.png")
+plt.xticks(plt.xticks()[0][::2]) # jeden zweiten Tick löschen
+#plt.yticks(plt.yticks()[0][::2])
 plt.subplots_adjust(left=0.07,right=0.835,top=0.98,bottom=0.10)
 f.legend(prop={'size': fontsize},loc="upper right", ncol=1)
 plt.show()
